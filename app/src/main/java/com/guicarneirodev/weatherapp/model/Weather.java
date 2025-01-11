2bb16c2bdb6de65ae2fe55a700cb3d585f8cbc7c
Guilherme <guicarneiro.dev@gmail.com>
2025-01-11 18:25:02 -0300

    odeio java

--- app/src/main/java/com/guicarneirodev/weatherapp/model/Weather.java
+++ app/src/main/java/com/guicarneirodev/weatherapp/model/Weather.java
@@ -1,4 +1,16 @@
 package com.guicarneirodev.weatherapp.model;
 
 public class Weather {
+    private Long id;
+    private String cityName;
+    private Double temperature;
+    private Integer humidity;
+    private Double windSpeed;
+    private String description;
+    private Double feelsLike;
+    private Integer pressure;
+    private Integer rainProbability;
+    private String weatherIcon;
+    private Long favoriteCityId;
+    private String createdAt;
 }
