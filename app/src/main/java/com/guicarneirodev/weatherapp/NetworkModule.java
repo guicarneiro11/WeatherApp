9f9164fe282fb768821e8aa7b7f520eee93a8b5b
Guilherme <guicarneiro.dev@gmail.com>
2025-01-12 14:13:22 -0300

    configurando Hilt para DI

--- app/src/main/java/com/guicarneirodev/weatherapp/NetworkModule.java
+++ app/src/main/java/com/guicarneirodev/weatherapp/NetworkModule.java
@@ -1,11 +1,43 @@
 package com.guicarneirodev.weatherapp;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 @InstallIn(SingletonComponent.class)
 public class NetworkModule {
 
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(new HttpLoggingInterceptor()
+                        .setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(OkHttpClient client) {
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 }
